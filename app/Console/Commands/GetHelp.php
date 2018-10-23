5b847735ef150d3da35b38e19c2c03760e57bf62
TJ Webb <tj@norex.ca>
2018-10-23 11:45:21 -0300

    initial help info

---
+++ app/Console/Commands/GetHelp.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GetHelp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cm:help';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Displays help about the toolset';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        echo "php artisan cm:site {domain}\n";
+        echo "\tThis will setup a new site, configure nginx, and restart.\n";
+    }
+}
